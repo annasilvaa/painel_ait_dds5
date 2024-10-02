9a1337680836c0e9063c21d256d5b1b19124ed89
ANNA JULYA DOS SANTOS DA SILVA <a.silva@labcetec.net>
2024-10-02 16:58:51 -0300

    Criando barra de navegação

--- client/src/components/pages/GestaoAulas.jsx
+++ client/src/components/pages/GestaoAulas.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import Navbar from '..layout/Navbar';
 
 function GestaoAulas() {
   return (
-    <div>GestaoAulas</div>
+    <div>
+      <Navbar/>
+      GestaoAulas
+      </div>
   )
 }
 
